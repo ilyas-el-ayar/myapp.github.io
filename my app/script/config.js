8228786e4c9457b801b5c9dc378705e3a017e266
ilyas-el-ayar <ilyaselayar155@gmail.com>
2024-11-03 14:22:39 +0100

    Add files via upload

---
+++ my app/script/config.js
@@ -0,0 +1,2 @@
+const destination = []
+export{destination}
---
+++ my app/script/script.js
@@ -0,0 +1,7 @@
+import { destination } from "./config";
+for(let i = 0; i < destination.length; i++){
+   let depart = document.getElementById("depart")
+   if (depart === destination.length){
+    
+   }
+}
